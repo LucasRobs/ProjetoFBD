cdb889e5232ca42f823172a989b29f3adcdc4905
LucasRobs <lucasrobs@alu.ufc.br>
2020-07-27 18:05:40 -0300

    img

--- frontend/src/pages/addProduto/index.js
+++ frontend/src/pages/addProduto/index.js
@@ -8,17 +8,57 @@ export default function Logon(){
   const[precoProduto, setprecoProduto] = useState('');
   const[descricaoProduto, setdescricaoProduto] = useState('');
   const[pesoProduto, setpesoProduto] = useState('');
-  const[imgProduto, setimgProduto] = useState('');
   const[nomeTipo, setnomeTipo] = useState('');
+  const[imgProdutolocal, setimgProdutolocal] = useState('');
+  var imgProduto = null;
   
   const history = useHistory();
+  const CLIENT_ID = '33e6bf969c0a62a';
+  const gallery = document.getElementById('gallery');
+  const doUpload = (url, options) => {
+    const promiseCallback = (resolve, reject) => {
 
+      const getFetchJson = (response) => {
+        if(!response.ok) return reject(response);
+        return response.json().then(resolve);
+      }
+
+      fetch(url, options)
+        .then(getFetchJson)
+        .catch(reject);
+    };
+    return new Promise(promiseCallback);
+  };
+
+  const onSuccess = (result) => {
+    const { data: { link } } = result;
+        //addImage(link);
+        imgProduto = link;
+    };
+/*
+  const addImage = (url) => {
+    gallery.innerHTML += `<img src="${url}" width="300" />`; 
+
+  }
+*/
   async function handleProduto(e){
     e.preventDefault();
-      try{
+    const file = document.getElementById('file');
+    const data = new FormData();
+    data.append('image', file.files[0]);
+
+    await doUpload('https://api.imgur.com/3/image', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Authorization': `Client-ID ${CLIENT_ID}`,
+      }
+    }).then(onSuccess)
+    .catch(console.error);
+    try{
         const response = await api.post('Produtos', { nomeProduto, precoProduto, descricaoProduto, pesoProduto, imgProduto, nomeTipo });
         alert(`deu foi certo mamae hahaha`);
-        history.push('/'); 
+        //history.push('/'); 
     }catch(err){
       alert('falha!!! ai meu deus papai ;-;')
     }
@@ -66,11 +106,12 @@ export default function Logon(){
                     <div class="field-group">
                         <div class="field">
                             <label for="img">imagem:</label>
-                            <input type="text" 
-                            name="img" 
+                            <input type="file" 
+                            name="file" 
+                            id="file" 
                             placeholder="Procurar documento" 
-                            value={imgProduto}  
-                            onChange={e => setimgProduto(e.target.value)}
+                            value={imgProdutolocal}  
+                            onChange={e => setimgProdutolocal(e.target.value)}
                             required/>
                         </div>
                         <div class="field">
--- frontend/src/pages/produtos/index.js
+++ frontend/src/pages/produtos/index.js
@@ -10,70 +10,75 @@ export default function Produtos(){
   const [produtos , setprodutos] =  useState([]);
    
   const nome = localStorage.getItem('nome');
-  const SessionId = localStorage.getItem('SessionId.data.idAdmin');
+  const SessionId = localStorage.getItem('SessionId');
 
   useEffect(() => {
-    api.get('',{
+    api.get('Produtos',{
       headers: {
         Authorization: SessionId,
       }
     }).then(response => {
-      setprodutos(response.data);
+      setprodutos(response.data); 
     })
   }, [SessionId]);
-  /*
+  
   async function handleDeleteIncident(id){
-    try{
-      await api.delete(`incidents/${id}`,{
-        headers:{
-          Authorization: SessionId,
+    if (window.confirm('Tem certeza que quer apagar isso?!')) {
+      await api.delete(`Produtos/${id}`);
+      try{
+          setprodutos(produtos.filter(produtos => produtos.idProduto !== id));
+        }catch(err){
+          alert('erro de deletar caso');
+          
         }
-      });
-      setprodutos(incidents.filter(incidents => incidents.id !== id));
-    }catch(err){
-      alert('erro de deletar caso');
-    
+    }else{
+      alert("cancelado");
     }
   }
-*/
+
+  async function handleEdit(id){
+    //history.push('/editar'); 
+  }
+
   return(
     <div id="page-produtos">
       <header>
           <img src={logoImg} alt="logomarca"/>
-          <a href="addProdutos">
+          <a href="addProduto">
               <FiPlusCircle color="#3A2C51"/>
           </a>
       </header>
   <h6>Bem Vindo ao sistema de administração { nome }</h6>
         <div class="produtos">
           <ul>
-          {produtos.map(produto => (
-            <li key={produto.idProduto}>
+          {produtos.map(produtos => (
+            <li key={produtos.idProduto}>
                 <img src={foto} alt=""/>
                 <div class="menu">
                 <div class="field">
-                  <strong>Cod: <h7> {produto.idProduto}</h7></strong>
-                  <strong>Nome: <h7>{produto.nomeProduto}</h7></strong>
+                  <strong>Cod: <h7> {produtos.idProduto}</h7></strong>
+                  <strong>Nome: <h7>{produtos.nomeProduto}</h7></strong>
                 <div class="field-group">
-                  <strong>Preço: <h7> {produto.precoProduto}</h7></strong>
-                  <strong>Peso: <h7> {produto.pesoProduto}</h7></strong>
+                  <strong>Preço: <h7> {produtos.precoProduto}</h7></strong>
+                  <strong>Peso: <h7> {produtos.pesoProduto}</h7></strong>
                 </div>
                 </div>
                 <div class="botoes">
                   <div class="apagar">
-                    <a href="apagar">
+    
+                    <button onClick={() => handleDeleteIncident(produtos.idProduto)} type="button">
                         <FiTrash2 color="#3A2C51"/>
-                    </a>
+                    </button>
                   </div>
                   <div class="editar">
-                    <a href="editar" class="editar">
-                        <FiEdit  color="#3A2C51"/>
-                    </a>
+                  <button onClick={() => handleEdit(produtos.idProduto)} type="button">
+                          <FiEdit  color="#3A2C51"/>
+                    </button>
                   </div>
                 </div>
                 </div>
             </li>
-          ))};
+          ))}
           </ul>
         </div>
   </div>
